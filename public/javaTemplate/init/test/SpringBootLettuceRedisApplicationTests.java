564b37b8ef6ad04c5bf66083b827565bddbf8797
chenjianhua <2290910211@qq.com>
2019-07-01 14:56:10 +0800

    :apple:

--- public/javaTemplate/init/test/SpringBootLettuceRedisApplicationTests.java
+++ public/javaTemplate/init/test/SpringBootLettuceRedisApplicationTests.java
@@ -36,19 +36,21 @@ public class SpringBootLettuceRedisApplicationTests {
     public void get() {
         // 测试线程安全
         ExecutorService executorService = Executors.newFixedThreadPool(1000);
+        String testIdKey = "test_userId";
+        String testName = "test_name";
         //主键生成
-        stringRedisTemplate.opsForValue().set("userId", "10000");
-        IntStream.range(0, 1000).forEach(i ->
-                executorService.execute(() -> stringRedisTemplate.opsForValue().increment("userId", 1))
+        stringRedisTemplate.opsForValue().set(testIdKey, "10000");
+        IntStream.range(0, 100).forEach(i ->
+                executorService.execute(() -> stringRedisTemplate.opsForValue().increment(testIdKey, 1))
         );
         // 简单key value获取
-        String userId = stringRedisTemplate.opsForValue().get("userId");
+        String userId = stringRedisTemplate.opsForValue().get(testIdKey);
         log.info("[主键生成userId] - [{}]", userId);
-        stringRedisTemplate.opsForValue().set("name", "fang");
-        String name = stringRedisTemplate.opsForValue().get("name");
+        stringRedisTemplate.opsForValue().set(testName, "fang");
+        String name = stringRedisTemplate.opsForValue().get(testName);
         log.info("[字符缓存结果] - [{}]", name);
         //  以下只演示整合，具体Redis命令可以参考官方文档，Spring Data Redis 只是改了个名字而已，Redis支持的命令它都支持
-        String userIdKey = "user:" + userId;
+        String userIdKey = "testUser:" + userId;
         redisCacheTemplate.opsForValue().set(userIdKey, "fangfang");
         // 对应 String（字符串）
         String fangName = (String) redisCacheTemplate.opsForValue().get(userIdKey);
